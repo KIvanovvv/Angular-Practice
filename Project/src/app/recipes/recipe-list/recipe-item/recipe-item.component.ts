529e51be28a9a30fd3743956eeaa6a6707047338
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-04-28 22:49:32 +0300

    Passing data from recipe-item to recipe-details component

--- Project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent {
+  @Input() recipe: Recipe;
+  @Output() sendSelectedRecipe = new EventEmitter<Recipe>();
 
+  onSelectRecipe() {
+    this.sendSelectedRecipe.emit(this.recipe);
+  }
 }
--- Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Test Recipe',
@@ -14,9 +15,17 @@ export class RecipeListComponent {
       'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
     ),
     new Recipe(
-      'Test Recipe',
+      'Test Recipe2',
+      'This is a test recipe',
+      'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
+    ),
+    new Recipe(
+      'Test Recipe3',
       'This is a test recipe',
       'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'
     ),
   ];
+  onSelectedRecipe(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
+  }
 }
