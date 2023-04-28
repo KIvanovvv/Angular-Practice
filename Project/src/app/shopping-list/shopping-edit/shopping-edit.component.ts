130b0b7d07a8162a64c8c39b71590621ca3dadc2
Krasimir Ivanov <k.ivanov29@yahoo.com>
2023-04-28 23:30:46 +0300

    Adding functionality to add new ingredients

--- Project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.modal';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
-
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @Output() addedItem = new EventEmitter<Ingredient>();
+  onAddItem(amountInput: HTMLInputElement) {
+    this.addedItem.emit(
+      new Ingredient(
+        this.nameInputRef.nativeElement.value,
+        Number(amountInput.value)
+      )
+    );
+    this.nameInputRef.nativeElement.value = '';
+    amountInput.value = '';
+  }
 }
