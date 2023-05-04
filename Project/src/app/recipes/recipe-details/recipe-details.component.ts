550bb638aeca58feefc4fe800fdbd22879e99417
unknown <k.ivanov29@yahoo.com>
2023-05-04 18:05:42 +0300

    Adding items from recipe to shopping list with Service

--- Project/src/app/recipes/recipe-details/recipe-details.component.ts
+++ Project/src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,18 +1,18 @@
-import { Component,Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipes.service';
+import { ShoppingService } from 'src/app/shopping-list/shopping.service';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  styleUrls: ['./recipe-details.component.css'],
 })
 export class RecipeDetailsComponent {
- @Input() recipe: Recipe;
-//  constructor(private recipeService: RecipeService) {
-//     // this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-//     //     this.recipe = recipe;
-//     //   });
-//  }
-
+  @Input() recipe: Recipe;
+  constructor(private shoppingService: ShoppingService) {}
+  onToShoppingList() {
+    this.recipe.ingredients.forEach((ingredient) => {
+      this.shoppingService.onAddItem(ingredient);
+    });
+  }
 }
--- Project/src/app/shopping-list/shopping.service.ts
+++ Project/src/app/shopping-list/shopping.service.ts
@@ -1,17 +1,24 @@
-import { Ingredient } from "../shared/ingredient.modal";
-import { EventEmitter } from "@angular/core";
+import { Ingredient } from '../shared/ingredient.modal';
+import { EventEmitter } from '@angular/core';
 
-export class ShoppingService{
-    ingredientChanged = new EventEmitter<Ingredient[]>();
-    private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10),
-      ];
-   getIngredients(){
-         return this.ingredients.slice();
-   }
-    onAddItem(ingredient: Ingredient) {
-        this.ingredients.push(ingredient);
-        this.ingredientChanged.emit(this.ingredients.slice());
-      }
+export class ShoppingService {
+  ingredientChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  onAddItem(ingredient: Ingredient) {
+    let found = this.ingredients.find((ing) => {
+      return ing.name === ingredient.name;
+    });
+    if (found) {
+      found.amount += ingredient.amount;
+    } else {
+      this.ingredients.push(ingredient);
+    }
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
 }
